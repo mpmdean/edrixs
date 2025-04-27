c5a47a648acb32d12a9ff2169d2ba7a593db75e8
Mark Dean <mpmdean@gmail.com>
2025-04-27 09:46:33 -0400

    Old test new

--- edrixs/tests/test_fock.py
+++ edrixs/tests/test_fock.py
@@ -6,6 +6,49 @@ def test_get_fock_full_N():
     assert edrixs.get_fock_full_N(4, 2) == [3, 5, 6, 9, 10, 12]
 
 
+def get_fock_half_N(N):
+    res = [[] for i in range(N + 1)]
+    for i in range(2**N):
+        occu = bin(i).count('1')
+        res[occu].append(i)
+    return res
+
+
+def get_fock_full_N_old(norb, N):
+    """
+    The slower function only works for even norb.
+    Use this to test the revised function.
+
+    Parameters
+    ----------
+    norb: int
+        Number of orbitals.
+    N: int
+        Number of occupancy.
+
+    Returns
+    -------
+    res: list of int
+        The decimal digitals to represent Fock states.
+    """
+
+    res = []
+    half_N = get_fock_half_N(norb // 2)
+    for m in range(norb // 2 + 1):
+        n = N - m
+        if n >= 0 and n <= norb // 2:
+            res.extend([i * 2**(norb // 2) + j
+                        for i in half_N[m] for j in half_N[n]])
+    return res
+
+
+def test_old_new():
+    for norb in [6, 8, 10]: # only test even norb
+        N = norb - 3
+        assert (sorted(get_fock_full_N_old(norb, N)) ==
+                sorted(edrixs.get_fock_full_N(norb, N)))
+
+
 def binary_from_full(norb, N):
     """Helper function, which generates binary
     from the fock_bin function."""
