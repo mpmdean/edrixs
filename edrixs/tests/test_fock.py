335caa55fdaf46546f74ac5d45c21d143052fb69
Mark Dean <mpmdean@gmail.com>
2025-04-26 20:32:46 -0400

    update test

--- edrixs/tests/test_fock.py
+++ edrixs/tests/test_fock.py
@@ -1,13 +1,14 @@
 import edrixs
 
 
-def test_fock():
-    """Confirm that fast version of the function behaves
-    the same as the more easily interpretable slow version."""
+def test_get_fock_full_N():
+    """Reproduce known behavior of get_fock_full_N"""
     assert edrixs.get_fock_full_N(4, 2) == [3, 5, 6, 9, 10, 12]
 
 
 def binary_from_full(norb, N):
+    """Helper function, which generates binary
+    from the fock_bin function."""
     return [int(''.join(str(b) for b in binary), 2)
             for binary in edrixs.fock_bin(norb, N)]
 
